b80522596ca3f38063ad204a10f8b01780fa4872
Xfouriour <xfouriour@gmail.com>
2023-04-26 19:45:25 +0500

    Deleted obvious comment

--- SimpleArchiver/Program.cs
+++ SimpleArchiver/Program.cs
@@ -7,9 +7,6 @@ namespace SimpleArchiver
     {
         static void Main(string[] args)
         {
-            //работа приложения начинается с запуска интерфейса
-            //который затем уже осуществляет управление:
-            //сжатие, распаковка и, может быть, подсчет чексуммы
             while(ConsoleInterface.ConsoleRequest() != 0);
         }
     }
